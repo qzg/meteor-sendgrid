378fef2d4fcc72cb904def85e5da239bc5915bbd
Kiyu Gabriel <q@qzg.us>
2013-07-30 23:45:17 -0500

    Initial Commit

---
+++ package.js
@@ -0,0 +1,9 @@
+Package.describe({
+	summary: "sendgrid NPM module packaged for Meteor."
+});
+
+Npm.depends({ 'sendgrid': '0.3.0' });
+
+Package.on_use(function (api) {
+	api.add_files('sendgrid.js', 'server');
+});
